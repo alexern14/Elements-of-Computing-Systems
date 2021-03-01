b466eae219831d737cb3f1f0c4523f906b30fdf5
alexern14 <aernst@mymail.mines.edu>
2021-02-28 21:05:44 -0700

    remove unwanted files

--- Project6/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/parallels/CSCI-410-Computing-systems/Project6
-# Build directory: /home/parallels/CSCI-410-Computing-systems/Project6/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
